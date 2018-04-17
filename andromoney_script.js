96649000131d6fef0974309569d66002b07acb88
Ricardo Hernandez <ricardo.a.hernandez@disney.com>
2018-04-17 15:12:36 -0700

    Updates to logic

--- andromoney_script.js
+++ andromoney_script.js
@@ -26,16 +26,37 @@ var MONTHS = [
   'Dec'
 ];
 
-
-var TRACKER_ID = '1AweSw57sw-a33ijYq9n7aQOR3xuizXeJoYExPKfMTQE';
+//2016_ID
+//var TRACKER_ID = '1AweSw57sw-a33ijYq9n7aQOR3xuizXeJoYExPKfMTQE';
+//2017_ID
+//var TRACKER_ID = '1NZWrWFLTJYJmvA_T0gq4--7DJc1L_GaF5XcS0N3Pgps';
+// Europe_2017
+//var TRACKER_ID = '1FyMr67nUW6yFL8NJFpguUCft9Q6f5RbC4CcsTIXfuME';
+//2018 ID
+var TRACKER_ID = '16ch-qMh1XGIVHZkRoAA4Rw4HUNqe70oigoGiVNyu8JQ';
 
 var MAX_ROW_NUMBER = 200;
 var MXN_CURRENCY = 'MXN';
+var currencies = {
+  'MXN': 'MXN',
+  'EUR': 'EUR',
+  'HUF': 'HUF',
+  'CZK': 'CZK'
+};
+
+var exchangeRate = {
+  'MXN': '18.6',
+  'EUR': '0.94',
+  'HUF': '290.52',
+  'CZK': '25.45'
+};
 
 var spreadsheet;
 var formattedSheet;
 var sheet;
-var exchangeRate = null;
+var currentMonth;
+
+// Application Triggers
 
 function onOpen() {
   spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
@@ -48,15 +69,31 @@ function onOpen() {
   spreadsheet.setActiveSheet(sheet);
 
   var menuEntries = [
-    {name: "Format & Transfer Data", functionName: "formatAndTransferData"}
+    {name: "Format & Transfer Data", functionName: "formatAndTransferData"},
+    {name: "Format Data", functionName: "formatData"},
   ];
   spreadsheet.addMenu("AndroidMoney", menuEntries);
 }
+
+function onEdit(e) {
+    var user = e.user;    
+    var sessionUser = Session.getActiveUser().getEmail();
+    // formatAndTransferData();
+    SpreadsheetApp.getActiveSpreadsheet().toast('Edit made by ' + user, 'Edit', 5);
+    formatData();
+}
+
+// Application
 
 function formatAndTransferData() {
   transformAmount();
   copyFormattedData();
   copyDataToTracker();
+}
+
+function formatData() {
+  transformAmount();
+  copyFormattedData();
 }
 
 /*
@@ -95,29 +132,29 @@ function transformAmount() {
         }
 
         // Open prompt
-        if (currency === MXN_CURRENCY) {
-          if (exchangeRate === null) {
+        if (currency !== 'USD' && currencies[currency] !== null) {
+          if (exchangeRate[currency] === null) {
             // open prompts
             var ui = SpreadsheetApp.getUi();
             var response = ui.prompt('Exchange Rate', 'What is current exchange rate?', ui.ButtonSet.OK_CANCEL);
 
             if (response.getSelectedButton() == ui.Button.OK) {
-              exchangeRate = response.getResponseText();
-              transformAmountToExchangeRate(cell);
+              exchangeRate[currency] = response.getResponseText();
+              transformAmountToExchangeRate(cell, exchangeRate[currency]);
             } else if (response.getSelectedButton() == ui.Button.CANCEL) {
              Logger.log('The user canceled the dialog.');
             } else {
              Logger.log('The user closed the dialog.');
             }
           } else {
-            transformAmountToExchangeRate(cell);
+            transformAmountToExchangeRate(cell, exchangeRate[currency]);
           }
         }
     }
 }
 
-function transformAmountToExchangeRate(cell) {
-  cell.setValue(cell.getValue / exchangeRate);
+function transformAmountToExchangeRate(cell, rate) {
+  cell.setValue(cell.getValue() / rate);
 }
 
 /*
@@ -132,6 +169,19 @@ function copyFormattedData() {
     var values = sheet.getRange(3, columnSource, MAX_ROW_NUMBER); //getRange(row, column, numRows)
     var targetColumn = HEADING_COLUMN[columnSource];
     values.copyValuesToRange(formattedSheet, targetColumn, targetColumn, 2, MAX_ROW_NUMBER);
+  }
+  
+  var lastRow = formattedSheet.getLastRow();
+
+  for (var i = 2; i <= lastRow; i++) {
+    var cell = formattedSheet.getRange('A' + i);
+    var dateString = cell.getValue() + '';
+    
+    var year     = dateString.substring(0,4);
+    var month    = dateString.substring(4,6);
+    var day      = dateString.substring(6,8);
+
+    cell.setValue(month + "/" + day + "/" + year);
   }
 }
 
@@ -159,14 +209,10 @@ function copyDataToTracker() {
 function getCurrentMonth() {
   spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   sheet = spreadsheet.getSheetByName('Formatted');
-
   var cell = sheet.getRange('A2');
-  var dateString = cell.getValue() + '';
-
-  var year        = parseInt(dateString.substring(0,4), 10);
-  var monthStr    = dateString.substring(4,6);
+  var date = Utilities.formatDate(cell.getValue(), spreadsheet.getSpreadsheetTimeZone(), "MM/dd/YY"); 
+  var monthStr    = date.substring(0,2);
   var month       = parseInt(monthStr, 10);
-  var day         = parseInt(dateString.substring(6,8), 10);
 
   return MONTHS[month];
 }
